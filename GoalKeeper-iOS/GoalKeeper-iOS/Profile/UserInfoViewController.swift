7a582189f942db59ac79c653a59c07cf26e13f1c
slr-09 <ge12090@naver.com>
2024-01-13 01:06:55 +0900

    Style: 회원정보 뷰 네비게이션 바

--- GoalKeeper-iOS/GoalKeeper-iOS/Profile/UserInfoViewController.swift
+++ GoalKeeper-iOS/GoalKeeper-iOS/Profile/UserInfoViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
+    let backButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        $0.tintColor = .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setNavigationBar()
+    }
+    
+    func setNavigationBar() {
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "회원정보"
+        let backButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.leftBarButtonItem = backButtonItem
     }
 }
