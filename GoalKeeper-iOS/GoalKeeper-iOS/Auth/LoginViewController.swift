746c60e113a3f0185ca54de94cc2d8b0f422ef9d
slr-09 <ge12090@naver.com>
2024-01-13 06:15:30 +0900

    Style: login 화면

---
+++ GoalKeeper-iOS/GoalKeeper-iOS/Auth/LoginViewController.swift
@@ -0,0 +1,93 @@
+//
+//  LoginViewController.swift
+//  GoalKeeper-iOS
+//
+//  Created by 가은 on 1/13/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let idTextField = UITextField().then {
+        $0.placeholder = "ID"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        $0.tintColor = .black
+    }
+    let pwTextField = UITextField().then {
+        $0.placeholder = "PW"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        $0.tintColor = .black
+        $0.isSecureTextEntry = true
+    }
+    let loginButton = UIButton().then {
+        $0.backgroundColor = UIColor(red: 1, green: 0.498, blue: 0, alpha: 1)
+        $0.setTitle("로그인", for: .normal)
+        $0.tintColor = .white
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+    }
+    let divider = UIView().then {
+        $0.backgroundColor = UIColor(red: 0.875, green: 0.875, blue: 0.875, alpha: 1)
+    }
+    let divider2 = UIView().then {
+        $0.backgroundColor = UIColor(red: 0.875, green: 0.875, blue: 0.875, alpha: 1)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        addView()
+        layoutConstraints()
+    }
+    
+}
+extension LoginViewController {
+    func addView() {
+        view.addSubview(idTextField)
+        view.addSubview(divider)
+        view.addSubview(pwTextField)
+        view.addSubview(divider2)
+        view.addSubview(loginButton)
+    }
+    
+    func layoutConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        let screenWidth = UIScreen.main.bounds.size.width
+        
+        idTextField.snp.makeConstraints { make in
+            make.width.equalTo(screenWidth*0.8)
+            make.height.equalTo(30)
+            make.center.equalTo(view.snp.center)
+        }
+        divider.snp.makeConstraints { make in
+            make.height.equalTo(1.5)
+            make.width.equalTo(screenWidth*0.8)
+            make.top.equalTo(idTextField.snp.bottom).offset(8)
+            make.centerX.equalTo(view.snp.centerX)
+
+        }
+        pwTextField.snp.makeConstraints { make in
+            make.width.equalTo(screenWidth*0.8)
+            make.height.equalTo(30)
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(divider.snp.bottom).offset(20)
+        }
+        divider2.snp.makeConstraints { make in
+            make.height.equalTo(1.5)
+            make.width.equalTo(screenWidth*0.8)
+            make.top.equalTo(pwTextField.snp.bottom).offset(8)
+            make.centerX.equalTo(view.snp.centerX)
+
+        }
+        loginButton.snp.makeConstraints { make in
+            make.width.equalTo(screenWidth*0.8)
+            make.height.equalTo(50)
+            make.top.equalTo(divider2.snp.bottom).offset(30)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+    }
+}
