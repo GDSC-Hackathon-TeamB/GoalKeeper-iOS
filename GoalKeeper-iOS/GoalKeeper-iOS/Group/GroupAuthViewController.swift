f99fe2a829795c69b71d77c6bfa45bc8cbc20ad6
hooni <jhlee909090@gmail.com>
2024-01-13 00:35:00 +0900

    gitflow-feature-stash: hooni

--- GoalKeeper-iOS/GoalKeeper-iOS/Group/GroupAuthViewController.swift
+++ GoalKeeper-iOS/GoalKeeper-iOS/Group/GroupAuthViewController.swift
@@ -4,14 +4,11 @@
 //
 //  Created by 이지훈 on 1/12/24.
 //
-//
 
 import UIKit
 
 class GroupAuthViewController: UIViewController {
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
