aa95ebb4f1a66c30426fd74bff2e287341b49a95
hooni <jhlee909090@gmail.com>
2024-01-12 21:05:44 +0900

    init Font

---
+++ GoalKeeper-iOS/GoalKeeper-iOS/Group/GroupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GroupViewController.swift
+//  GoalKeeper-iOS
+//
+//  Created by 이지훈 on 1/12/24.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
