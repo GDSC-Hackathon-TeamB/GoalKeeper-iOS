21f1a7fcfcb90429a1863b4dc7d4a8d1ffd38131
slr-09 <ge12090@naver.com>
2024-01-13 01:20:58 +0900

    Style: 회원정보 페이지 닉네임 뷰

--- GoalKeeper-iOS/GoalKeeper-iOS/Profile/UserInfoViewController.swift
+++ GoalKeeper-iOS/GoalKeeper-iOS/Profile/UserInfoViewController.swift
@@ -14,12 +14,30 @@ class UserInfoViewController: UIViewController {
         $0.tintColor = .black
     }
     
+    let nicknameLabel = UILabel().then {
+        $0.text = "닉네임"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .gray
+    }
+    
+    let nickname = UILabel().then {
+        $0.text = "nicknick"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        $0.textColor = .black
+    }
+    
+    let grayLine1 = UIView().then {
+        $0.backgroundColor = UIColor(red: 0.954, green: 0.954, blue: 0.954, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
         setNavigationBar()
+        addView()
+        layoutConstraints()
         
         buttonActions()
     }
@@ -37,5 +55,32 @@ class UserInfoViewController: UIViewController {
     
     @objc func didClickBackButton() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension UserInfoViewController {
+    func addView() {
+        view.addSubview(nicknameLabel)
+        view.addSubview(nickname)
+        view.addSubview(grayLine1)
+    }
+    
+    func layoutConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        nicknameLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeArea.snp.top).offset(40)
+            make.leading.equalTo(safeArea.snp.leading).offset(25)
+        }
+        nickname.snp.makeConstraints { make in
+            make.top.equalTo(nicknameLabel.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(25)
+        }
+        grayLine1.snp.makeConstraints { make in
+            make.height.equalTo(1.5)
+            make.width.equalTo(view.snp.width).offset(-50)
+            make.top.equalTo(nickname.snp.bottom).offset(8)
+            make.centerX.equalTo(view.snp.centerX)
+        }
     }
 }
