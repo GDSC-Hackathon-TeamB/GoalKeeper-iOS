7daa6b7d11a1377bd2e7d224e391dfe62790a59b
slr-09 <ge12090@naver.com>
2024-01-13 02:40:36 +0900

    Style: 자기계발탭-목표 설정 label

--- GoalKeeper-iOS/GoalKeeper-iOS/personal/SetGoalViewController.swift
+++ GoalKeeper-iOS/GoalKeeper-iOS/personal/SetGoalViewController.swift
@@ -14,14 +14,26 @@ class SetGoalViewController: UIViewController {
         $0.tintColor = .black
     }
     
+    let setGoalTitle = UILabel().then {
+        $0.text = "목표 설정하기"
+        $0.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        $0.textColor = .black
+    }
+    
+    let setGoalSubtitle = UILabel().then {
+        $0.text = "나의 목표를 간단하게 한 문장으로 표현해봐요"
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
         setNavigationBar()
-//        addView()
-//        layoutConstraints()
+        addView()
+        layoutConstraints()
         
         buttonActions()
         
@@ -40,5 +52,25 @@ class SetGoalViewController: UIViewController {
     
     @objc func didClickBackButton() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension SetGoalViewController {
+    func addView() {
+        view.addSubview(setGoalTitle)
+        view.addSubview(setGoalSubtitle)
+    }
+    
+    func layoutConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        setGoalTitle.snp.makeConstraints { make in
+            make.top.equalTo(safeArea.snp.top).offset(130)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+        setGoalSubtitle.snp.makeConstraints { make in
+            make.top.equalTo(setGoalTitle.snp.bottom).offset(5)
+            make.centerX.equalTo(view.snp.centerX)
+        }
     }
 }
